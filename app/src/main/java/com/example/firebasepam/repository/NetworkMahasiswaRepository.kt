2aafabacd227807167cee63804c34a91bc87751d
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-13 14:22:08 +0700

    menambahkan implementasi fungsi insert

--- app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
@@ -6,6 +6,7 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkMahasiswaRepository(
     private val firestore: FirebaseFirestore
@@ -27,19 +28,15 @@ class NetworkMahasiswaRepository(
     }
 
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .add(mahasiswa)
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal memanbah data mahasiswa: ${e.message}")
+        }
     }
 
-    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
 
-    override suspend fun deleteMahasiswa(nim: String) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
-    }
 
 }
