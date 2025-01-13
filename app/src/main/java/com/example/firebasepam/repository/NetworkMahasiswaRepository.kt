1a1f4a946706962afc9512798ce6e5064d690f20
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-13 14:24:39 +0700

    menambahkan implementasi fungsi get by nim

--- app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
@@ -59,6 +59,19 @@ class NetworkMahasiswaRepository(
         }
     }
 
+    override suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa> = callbackFlow{
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener { value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose {
+            mhsDocument.remove()
 
+        }
+    }
 
 }
