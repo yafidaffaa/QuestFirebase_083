b3e71900544159334d8d4563305779b14573ed0e
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-13 14:20:18 +0700

    menghilangkan parameter nim

--- app/src/main/java/com/example/firebasepam/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/firebasepam/repository/MahasiswaRepository.kt
@@ -8,9 +8,9 @@ interface MahasiswaRepository {
 
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
-    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 
-    suspend fun deleteMahasiswa(nim: String)
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
 
     suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa>
 }
