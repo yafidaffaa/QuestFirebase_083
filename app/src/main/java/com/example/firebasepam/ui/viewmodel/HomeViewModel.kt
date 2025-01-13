e75e682a30273b97e56f6ccfacf9c55e28261e51
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-13 14:26:51 +0700

    menambahkan fungsi delete pada home view model

--- app/src/main/java/com/example/firebasepam/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/HomeViewModel.kt
@@ -43,18 +43,16 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
                 }
         }
     }
-}
 
-//    fun deleteMhs(nim: String) {
-//        viewModelScope.launch {
-//            try {
-//                mhs.deleteMahasiswa(nim)
-//            } catch (e: IOException) {
-//                HomeUiState.Error
-//            } catch (e: HttpException) {
-//                HomeUiState.Error
-//            }
-//        }
-//    }
+    fun deleteMhs(mahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(mahasiswa)
+            } catch (e: Exception) {
+                mhsUIState = HomeUiState.Error(e)
+            }
+        }
+    }
+}
 
 
