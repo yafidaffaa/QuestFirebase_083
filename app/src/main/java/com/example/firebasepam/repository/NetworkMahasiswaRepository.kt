4bef5459ea9ef12b7c4e444b12584addf1ada3f6
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-13 14:23:55 +0700

    menambahkan implementasi fungsi delete

--- app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebasepam/repository/NetworkMahasiswaRepository.kt
@@ -48,6 +48,17 @@ class NetworkMahasiswaRepository(
         }
     }
 
+    override suspend fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
+        }
+    }
+
 
 
 }
